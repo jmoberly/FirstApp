4d67e2fc014f66367c9b8366ad0921fb55a296cf
jmoberly <jmoberly@tradestation.com>
2013-09-04 14:39:47 -0500

    Added customer list

---
+++ FirstApp/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Description { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
