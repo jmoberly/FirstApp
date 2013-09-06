68868dbb3b5f27787d0806514362720105feec30
jmoberly <jmoberly@tradestation.com>
2013-09-06 12:22:36 -0500

    Finished chapter 5

---
+++ FirstApp/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FirstApp.Models;
+
+namespace FirstApp.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
